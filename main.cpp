83934222a66dd30a6961065c04e3420cfc206e5f
Eva <kotenkoeva36@gmail.com>
2025-10-26 23:25:27 +0200

    first load

---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "stdio.h"
+#include <windows.h>
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int a = 3, c = 5;
+    double b = sqrt(c * c - a * a);
+    double S = a * b * 0.5;
+    cout << "Катет b = " << b << endl;
+    cout << "Площа  S = " << S << endl;
+    return 0;
+}
+
