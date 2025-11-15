2972f5e8246d6bda2da7a9f2cd6985be79457f06
Eva <kotenkoeva36@gmail.com>
2025-11-15 22:48:34 +0200

    Змінені файли

--- lab7.cpp
+++ lab7.cpp
@@ -1,11 +1,8 @@
 ﻿#include <iostream>
-#include "windows.h"
 using namespace std;
 
 int main()
 {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
     int a;
     cout << "Введіть тризначне число: "; 
     cin >> a;
