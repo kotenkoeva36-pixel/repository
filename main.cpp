d7a47017baf69ae1dfcfa9a4e05d34cf1f7aa734
Eva <kotenkoeva36@gmail.com>
2025-11-23 22:07:17 +0200

    змііни

--- main.cpp
+++ main.cpp
@@ -8,8 +8,6 @@ int main()
     int a = 3, c = 5;
     double b = sqrt(c * c - a * a);
     double S = a * b * 0.5;
-    cout << "Катет b = " << b << endl;
-    cout << "Площа  S = " << S << endl;
     return 0;
 }
 
